0b412ac0fa84504a117ec3b1660fa98aba82c3c6
el-dani-cortes <danicor83@gmail.com>
2021-01-17 19:35:45 -0500

    Changed file to executable format

---
+++ level_0/vote_level_0.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Program to upload 1024 votes to level_0
+of hodor project"""
+import requests
+
+
+# Request web page by get method
+url = 'http://158.69.76.135/level0.php'
+my_id = '2304'
+params = {'id': my_id, 'holdthedoor': 'Submit'}
+vote_success = 'Hold the Door challenge - Level 0'
+r = requests.get(url)
+
+# Loop to make the votes by post method
+print("Uploading votes...")
+vote = 1
+while vote <= 1024:
+    r = requests.post(url, data=params)
+    if r.status_code is 200 and vote_success in r.text:
+        print("Upload vote #{}".format(vote), end='\r', flush=True)
+        vote += 1
+    else:
+        print("Fail vote #{}".format(vote), end='\r', flush=True)
+print("cheat online voting process ends :)")
